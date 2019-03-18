7a6070d5c1eff44b0f034ed0ead50caa3498fcc2
digital-thinking <github@digital-thinking.de>
2019-03-18 14:22:26 +0100

    add mcc

--- src/main/java/de/ixeption/classify/binary/BinaryUtils.java
+++ src/main/java/de/ixeption/classify/binary/BinaryUtils.java
@@ -1,7 +1,6 @@
 package de.ixeption.classify.binary;
 
 import smile.validation.ClassificationMeasure;
-import smile.validation.ConfusionMatrix;
 
 public class BinaryUtils {
 
@@ -39,34 +38,6 @@ public class BinaryUtils {
         @Override
         public String toString() {
             return m[0][0] + "\t" + m[0][1] + "\n" + m[1][0] + "\t" + m[1][1];
-        }
-    }
-
-    public static class MatthewsCorrelationCoefficient implements ClassificationMeasure {
-
-        public double measure(int[] truth, int[] prediction) {
-            if (truth.length != prediction.length) {
-                throw new IllegalArgumentException(String.format("The vector sizes don't match: %d != %d.", truth.length, prediction.length));
-            }
-            ConfusionMatrix confusionMatrix = new ConfusionMatrix(truth, prediction);
-            int[][] matrix = confusionMatrix.getMatrix();
-
-            if (matrix.length != 2 || matrix[0].length != 2) {
-                throw new IllegalArgumentException("MCC can only be applied to binary classification: " + confusionMatrix.toString());
-            }
-
-            int tp = matrix[0][0];
-            int tn = matrix[1][1];
-            int fp = matrix[0][1];
-            int fn = matrix[1][0];
-
-            int nominator = (tp * tn - fp * fn);
-            double denominator = Math.sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn));
-
-            if (nominator == 0 || denominator == 0) return 0;
-
-            return nominator / denominator;
-
         }
     }
 
